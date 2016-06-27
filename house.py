65c0361a4e44b5b6d1f0cb4026f8013b120c1b07
Ara Yaghsizian <ayaghsizian@gmail.com>
2016-06-27 11:15:35 -0700

    Update house.py
    I love tinkering with things and I'm sure I'll update this 100 more times.

--- house.py
+++ house.py
@@ -1,4 +1,11 @@
-print
+'''
+This program was created by myself, Ara Yaghsizian, to ballpark future house payments.
+As I was searching for houses I noticed the estimated payments were always so low.
+This was due to the payment calculators only calculating the principal and interest of the mortgage payments.
+In real life we have to pay taxes and fire insurance and sometimes PMI which can increase the costs dramatically.
+We usually don't know the exact costs of these but we can estimate pretty closely.
+Here goes!
+'''
 print
 print "ROZARA\'S HOUSE"
 print
@@ -8,18 +15,18 @@ print
 print "A house payment calculator... please use the examples as a guide."
 print
 house_price = float(raw_input("What is the house price? (example 200000): "))
-interest_rate = float(raw_input("What is the interest rate (example .035): "))
+interest_rate = float(raw_input("What is the interest rate (example .035 (not 3.5%)): "))
 down_payment = float(raw_input("What is the down payment (example 25000): "))
 mortgage = float(house_price) - float(down_payment)
 
-principal_monthly = mortgage/600
+principal_monthly = mortgage/600 #estimated for simplicity
 interest_monthly = mortgage * interest_rate / 12
-taxes_monthly = house_price/1000 
-insurance_monthly = house_price/2000
-if mortgage <= house_price * .8:
+taxes_monthly = house_price * .02 / 12 #estimated for simplicity
+insurance_monthly = house_price/2000 #estimated for simplicity
+if mortgage <= house_price * .8: #Do you want to put 20% down to avoid PMI?
     monthly_payment = principal_monthly + interest_monthly + taxes_monthly + insurance_monthly
 else:
-    monthly_payment = principal_monthly + interest_monthly + taxes_monthly + insurance_monthly + 75
+    monthly_payment = principal_monthly + interest_monthly + taxes_monthly + insurance_monthly + 75 #estimated for simplicity
 print
 print "Monthly printcipal: ", principal_monthly
 print "Monthly interest: ", interest_monthly
